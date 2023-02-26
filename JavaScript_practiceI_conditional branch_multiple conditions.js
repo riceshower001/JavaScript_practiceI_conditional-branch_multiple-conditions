74def4bf2f2082d6e196522f8045eee40bd9306b
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-02-26 14:41:19 +0900

    Add files via upload

---
+++ JavaScript_practiceI_conditional branch_multiple conditions.js
@@ -0,0 +1,9 @@
+const age = 24;
+
+// Let's create an if statement using "&&".
+// Please create an if statement with the condition "The value of the constant "age" is 20 or more and less than 30.".
+// And please print the string "I'm in my twenties." using the if statement.
+
+if (age >= 20 && age < 30) {
+  console.log("I'm in my twenties.");
+}
